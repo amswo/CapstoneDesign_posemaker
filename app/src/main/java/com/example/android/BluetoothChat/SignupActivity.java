0c474d11a24e79b283c038564e71585f62547df7
amswo <201111212@sangmyung.kr>
2016-08-07 04:06:42 +0900

    first commit

---
+++ app/src/main/java/com/example/android/BluetoothChat/SignupActivity.java
@@ -0,0 +1,117 @@
+package com.example.android.BluetoothChat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * Created by user on 2016-08-07.
+ */
+public class SignupActivity extends Activity {
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etPasswordConfirm;
+    private Button btnDone;
+    private Button btnCancel;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etPasswordConfirm = (EditText) findViewById(R.id.etPasswordConfirm);
+        btnDone = (Button) findViewById(R.id.btnDone);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // 비밀번호 일치 검사
+        etPasswordConfirm.addTextChangedListener(new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = etPassword.getText().toString();
+                String confirm = etPasswordConfirm.getText().toString();
+
+                if (password.equals(confirm)) {
+                    etPassword.setBackgroundColor(Color.GREEN);
+                    etPasswordConfirm.setBackgroundColor(Color.GREEN);
+                } else {
+                    etPassword.setBackgroundColor(Color.RED);
+                    etPasswordConfirm.setBackgroundColor(Color.RED);
+                }
+            }
+
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // 이메일 입력 확인
+                if (etEmail.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Email을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 입력 확인
+                if (etPassword.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 확인 입력 확인
+                if (etPasswordConfirm.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "비밀번호 확인을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPasswordConfirm.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 일치 확인
+                if (!etPassword.getText().toString().equals(etPasswordConfirm.getText().toString())) {
+                    Toast.makeText(SignupActivity.this, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                    etPassword.setText("");
+                    etPasswordConfirm.setText("");
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                Intent result = new Intent();
+                result.putExtra("email", etEmail.getText().toString());
+
+                // 자신을 호출한 Activity로 데이터를 보낸다.
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
