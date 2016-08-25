459967d73258f27f84aba13c44f1caf00904da05
amswo <201111212@sangmyung.kr>
2016-08-26 01:14:10 +0900

    0825 1차완료

--- app/build/generated/source/r/debug/com/example/android/BluetoothChat/R.java
+++ app/build/generated/source/r/debug/com/example/android/BluetoothChat/R.java
@@ -834,8 +834,7 @@ containing a value of this type.
         public static final int text=0x7f020066;
     }
     public static final class id {
-        public static final int IvPoses=0x7f0d004a;
-        public static final int V_textView=0x7f0d0060;
+        public static final int V_textView=0x7f0d0057;
         public static final int action_bar=0x7f0d001c;
         public static final int action_bar_activity_content=0x7f0d0000;
         public static final int action_bar_container=0x7f0d001b;
@@ -849,26 +848,19 @@ containing a value of this type.
         public static final int action_mode_close_button=0x7f0d0024;
         public static final int activity_chooser_view_content=0x7f0d0025;
         public static final int always=0x7f0d0013;
-        public static final int app_textView=0x7f0d005d;
+        public static final int app_textView=0x7f0d0054;
         public static final int beginning=0x7f0d000f;
-        public static final int btnCompare=0x7f0d0044;
-        public static final int btnEmail=0x7f0d0047;
-        public static final int btnHome=0x7f0d0045;
-        public static final int btnInitialPose=0x7f0d0055;
-        public static final int btnPoseStart=0x7f0d0051;
-        public static final int btnPoseType=0x7f0d0056;
-        public static final int btnSelectAllDatas=0x7f0d0046;
-        public static final int btnSetting=0x7f0d0057;
-        public static final int btnStart=0x7f0d0043;
+        public static final int btnInitialPose=0x7f0d004c;
+        public static final int btnPoseStart=0x7f0d0048;
+        public static final int btnPoseType=0x7f0d004d;
+        public static final int btnSetting=0x7f0d004e;
         public static final int button_scan=0x7f0d0042;
-        public static final int buttonreset=0x7f0d0048;
         public static final int checkbox=0x7f0d002d;
-        public static final int chronometer=0x7f0d004b;
         public static final int collapseActionView=0x7f0d0014;
         public static final int default_activity_button=0x7f0d0028;
         public static final int dialog=0x7f0d0018;
         public static final int disableHome=0x7f0d0009;
-        public static final int discoverable=0x7f0d0062;
+        public static final int discoverable=0x7f0d0059;
         public static final int dropdown=0x7f0d0019;
         public static final int edit_query=0x7f0d0030;
         public static final int end=0x7f0d0010;
@@ -879,12 +871,12 @@ containing a value of this type.
         public static final int icon=0x7f0d002a;
         public static final int ifRoom=0x7f0d0015;
         public static final int image=0x7f0d0027;
-        public static final int imageView3=0x7f0d004e;
-        public static final int imageView4=0x7f0d004f;
-        public static final int imageView5=0x7f0d005c;
-        public static final int imageView6=0x7f0d005e;
-        public static final int initialpose_textView=0x7f0d004d;
-        public static final int initposebutton=0x7f0d0050;
+        public static final int imageView3=0x7f0d0045;
+        public static final int imageView4=0x7f0d0046;
+        public static final int imageView5=0x7f0d0053;
+        public static final int imageView6=0x7f0d0055;
+        public static final int initialpose_textView=0x7f0d0044;
+        public static final int initposebutton=0x7f0d0047;
         public static final int listMode=0x7f0d0006;
         public static final int list_item=0x7f0d0029;
         public static final int middle=0x7f0d0011;
@@ -893,11 +885,11 @@ containing a value of this type.
         public static final int none=0x7f0d0012;
         public static final int normal=0x7f0d0007;
         public static final int paired_devices=0x7f0d003f;
-        public static final int poseposition_view=0x7f0d0052;
+        public static final int poseposition_view=0x7f0d0049;
         public static final int progress_circular=0x7f0d0004;
         public static final int progress_horizontal=0x7f0d0005;
         public static final int radio=0x7f0d002f;
-        public static final int scan=0x7f0d0061;
+        public static final int scan=0x7f0d0058;
         public static final int search_badge=0x7f0d0032;
         public static final int search_bar=0x7f0d0031;
         public static final int search_button=0x7f0d0033;
@@ -908,14 +900,14 @@ containing a value of this type.
         public static final int search_plate=0x7f0d0036;
         public static final int search_src_text=0x7f0d0037;
         public static final int search_voice_btn=0x7f0d003b;
-        public static final int setting_textView=0x7f0d004c;
+        public static final int setting_textView=0x7f0d0043;
         public static final int shortcut=0x7f0d002e;
-        public static final int shoulder=0x7f0d0053;
+        public static final int shoulder=0x7f0d004a;
         public static final int showCustom=0x7f0d000b;
         public static final int showHome=0x7f0d000c;
         public static final int showTitle=0x7f0d000d;
-        public static final int sound_textView=0x7f0d0058;
-        public static final int sound_toggleButton=0x7f0d0059;
+        public static final int sound_textView=0x7f0d004f;
+        public static final int sound_toggleButton=0x7f0d0050;
         public static final int split_action_bar=0x7f0d001e;
         public static final int submit_area=0x7f0d0039;
         public static final int tabMode=0x7f0d0008;
@@ -925,13 +917,12 @@ containing a value of this type.
         public static final int title_paired_devices=0x7f0d003e;
         public static final int title_right_text=0x7f0d003d;
         public static final int top_action_bar=0x7f0d0020;
-        public static final int tv_Data=0x7f0d0049;
         public static final int up=0x7f0d0021;
         public static final int useLogo=0x7f0d000e;
-        public static final int version_textView=0x7f0d005f;
-        public static final int vibration_textView=0x7f0d005a;
-        public static final int vibration_toggleButton=0x7f0d005b;
-        public static final int waist=0x7f0d0054;
+        public static final int version_textView=0x7f0d0056;
+        public static final int vibration_textView=0x7f0d0051;
+        public static final int vibration_toggleButton=0x7f0d0052;
+        public static final int waist=0x7f0d004b;
         public static final int withText=0x7f0d0017;
     }
     public static final class integer {
@@ -966,12 +957,11 @@ containing a value of this type.
         public static final int custom_title=0x7f040019;
         public static final int device_list=0x7f04001a;
         public static final int device_name=0x7f04001b;
-        public static final int home=0x7f04001c;
-        public static final int initial_pose=0x7f04001d;
-        public static final int main=0x7f04001e;
-        public static final int message=0x7f04001f;
-        public static final int setting=0x7f040020;
-        public static final int support_simple_spinner_dropdown_item=0x7f040021;
+        public static final int initial_pose=0x7f04001c;
+        public static final int main=0x7f04001d;
+        public static final int message=0x7f04001e;
+        public static final int setting=0x7f04001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f040020;
     }
     public static final class menu {
         public static final int option_menu=0x7f0e0000;
